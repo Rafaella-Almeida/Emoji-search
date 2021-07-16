62508268ecac9bc96cc71da36e83a8382f02ee09
rafaella-almeida <rafaellaalmeida828@gmail.com>
2021-07-15 23:35:25 -0300

    filter concluido

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import "./App.css";
 import arrayEmoji from "./services/getEmoji";
 import ContainerEmoji from "./Components/ContainerEmoji";
+import { useState } from "react";
 
 function App() {
+  const [emoji, setEmoji] = useState("");
   return (
     <div>
       <div className="container-topo">
         <h1>😸 Emoji Search 😽</h1>
-        <input className="input"></input>
+        <input className="input" onChange={(e) => setEmoji(e.target.value)} />
       </div>
 
-      {arrayEmoji.map((i) => {
-        return <ContainerEmoji title={i.title} symbol={i.symbol} />;
-      })}
+      {arrayEmoji
+        .filter((item) =>
+          item.keywords.toLowerCase().includes(emoji.toLowerCase())
+        )
+        .map((i) => {
+          return <ContainerEmoji title={i.title} symbol={i.symbol} />;
+        })}
     </div>
   );
 }
